fcac031100785986ab257ff3bd8a5ab70027d96c
rsilva <rsilva@contruent.com>
2025-08-21 16:38:31 -0300

    add secrets variables

--- pages/loginPage.ts
+++ pages/loginPage.ts
@@ -1,5 +1,7 @@
 import { Page } from '@playwright/test'; //built-in Page object, which represents a single browser tab or window.
 
+
+
 export class LoginPage {
     readonly page: Page; //The readonly page: Page; ensures each instance of LoginPage has its own page context that we can’t accidentally overwrite.
                          //The property page must always be a Playwright Page object   
--- tests/login.test.ts
+++ tests/login.test.ts
@@ -1,14 +1,15 @@
 import { test } from '../fixtures/custom-fixtures';
 import { expect } from '@playwright/test';
 
+const username = process.env.LOGIN_USER!;
+const password = process.env.LOGIN_PASS!;
+const baseUrl  = process.env.BASE_URL!;
+
 
 test('Login should succeed with valid credentials', async ({ loginPage }) => {
-    await loginPage.navigateToLogin('standard_user', 'secret_sauce');
-    
-    
+    await loginPage.navigateToLogin(username, password);  
     // Verify successful login by checking the URL or a specific element on the page
-    
-    await expect(loginPage.page).toHaveURL('https://www.saucedemo.com/v1/inventory.html'); // Adjust the URL as needed
+    await expect(loginPage.page).toHaveURL(`${baseUrl}/inventory.html`); // Adjust the URL as needed
     await expect(loginPage.page.locator('.product_label', { hasText: 'Products' })).toBeVisible();
     
 });
